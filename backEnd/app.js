011414e78aeae6682b8b57a8aa9a9f6900d37b92
PY-MAD <98361381+PY-MAD@users.noreply.github.com>
2025-03-26 13:31:34 +0300

    fix probloms

--- backEnd/app.js
+++ backEnd/app.js
@@ -61,6 +61,6 @@ app.use("/tanqeeb",tanqeeb)
 app.use("/auth",auth)
 
 
-Server.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port", process.env.PORT || 3000);
 });
